d747bcc11254e4b73283f0df3ba42a7098ed2ef1
kacy hulme <kacyhulme@gmail.com>
2015-02-22 21:09:07 -0600

    add respond with json for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,8 @@
 class TweetsController < ApplicationController
+  respond_to :json, :html
+
   def index
     @tweet = current_user.twitter.search("rails", result_type: "recent").first
+    respond_with tweets: [{text: @tweet.text, user: current_user.name, id: 1}]
   end
 end
