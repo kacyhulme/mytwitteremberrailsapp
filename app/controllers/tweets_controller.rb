492d7e62fc29bb4833bcefbeef2e814d1b927649
kacy hulme <kacyhulme@gmail.com>
2015-02-22 20:03:04 -0600

    adds configuration for twitter auth

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = current_user.twitter.search("rails", result_type: "recent").take(3)
+    @tweet = current_user.twitter.search("rails", result_type: "recent").first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first || create_from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first || create_from_omniauth(auth)
+    user.oauth_token = auth["credentials"]["token"]
+    user.oauth_secret = auth["credentials"]["secret"]
+    user.save!
+    user
   end
 
   def self.create_from_omniauth(auth)
@@ -8,6 +12,17 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["nickname"]
+    end
+  end
+
+  def twitter
+    if provider == "twitter"
+      @twitter ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = "M4SjEcLHIpTY1CvaQGMTPq6aG"
+        config.consumer_secret     = "t7DUxNL8Kr6u8jsRzv9oGrBkd0VK7mLwDyennsB7wrpUWE2gex"
+        config.access_token        = oauth_token
+        config.access_token_secret = oauth_secret
+      end
     end
   end
 end
